259786495b89c9f8007fab6135fae6b57071e315
Masum-Arifin <masumarifinbd@gmail.com>
2022-03-17 18:17:30 +0600

    new code

--- js/app.js
+++ js/app.js
@@ -76,7 +76,7 @@ const displayPhoneDetails = (item) => {
       <h6 class="text-light"><span class="fw-bold text-light">Memory:</span> ${item.mainFeatures.memory}</h6>
       <h6 class="text-light"><span class="fw-bold text-light">Sensors:</span> ${item.mainFeatures.sensors}</h6>
       <h6 class="text-light"><span class="fw-bold text-light">storage:</span> ${item.mainFeatures.storage}</h6>
-      // <h4 class="fw-bold text-light"> Others</h4>
+      <h4 class="fw-bold text-light"> Others</h4>
       <h6 class="text-light"><span class="fw-bold text-light">Bluetooth:</span> ${item.others?.Bluetooth}</h6>
       <h6 class="text-light"><span class="fw-bold text-light">GPS:</span> ${item.others?.GPS}</h6>
       <h6 class="text-light"><span class="fw-bold text-light">NFC:</span> ${item.others?.NFC}</h6>
